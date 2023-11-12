a46b9106c0bfa5860ce3ed089cd54af6c19ef833
c0rdyc3p5 <kevinlecado@gmail.com>
2023-11-12 14:55:54 -0500

    Fix base path for relative link of assets

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,11 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  base: './',
   server: {
     hmr: false
   },
   build: {
     outDir: 'docs',
-    assetsDir: '.'
   }
 })
